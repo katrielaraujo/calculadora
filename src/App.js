b19f2c95863ec8c3030f2ca6db98bf61ac33f0fa
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2022-06-09 17:15:41 -0300

    alteração geral

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import logo from './images/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container-freecodecamp-logo'>
+        <img src={logo} className='freecodecamp-logo'
+        alt='logo do projeto'/>
+      </div>
+      <div className='calculadora-container'>
+        <div className='fila'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='fila'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='fila'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>x</Button>
+        </div>
+        <div className='fila'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
